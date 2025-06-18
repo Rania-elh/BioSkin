a1abc6e1fe7e2705556854f41fe119281291a80b
Rania-elh <rania1elhefnawy@gmail.com>
2025-06-18 19:05:06 +0200

    Update

---
+++ Backend/models/favoris.py
@@ -0,0 +1,4 @@
+class Favoris:
+    def __init__(self, user_id, recipe_id):
+        self.user_id = user.id
+        self.recipe_id = recipe_id
---
+++ Backend/models/user.py
@@ -0,0 +1,6 @@
+class User:
+    def __init__(self, username, email, password, skin_type_id):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.skin_type_id = skin_type_id
