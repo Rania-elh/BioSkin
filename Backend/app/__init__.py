9489c1489f685f28e64f42606929b0825dd87273
Kadiga6 <9510@holbertonstudents.com>
2025-06-18 17:41:41 +0200

    backend

---
+++ Backend/app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from .database.db import init_db
+from .routes import user_routes, ai_routes
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_object('config.py')
+    
+    init_db(app)
+
+    app.register_blueprint(user_routes.bp)
+    app.register_blueprint(ai_routes.bp)
+
+    return app
