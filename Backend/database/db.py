cb1af7a37a20ed90f3dd94e52c3839c19f26d8f7
Rania-elh <rania1elhefnawy@gmail.com>
2025-06-18 18:23:51 +0200

    Update

---
+++ Backend/database/db.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+# Charger les variables d'environnement depuis .env
+load_dotenv()
+# Récupérer l'URI de connexion
+MONGO_URI = os.getenv("MONGO_URI")
+# Créer la connexion
+client = MongoClient(MONGO_URI)
+# Choisir la base de données
+db = client["bioskin"]
+# Exemple d'accès à une collection
+users_collection = db["users"]
+recipes_collection = db["recipes"]
+favorites_collection = db["favorites"]
+ingredients_collection = db["ingredients"]
+zone_collection = db["zone"]
+skin_types_collection = db["skin_type"]
