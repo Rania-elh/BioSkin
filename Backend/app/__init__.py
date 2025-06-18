527dcf023e3d4316d23fc1c1362a3bbaf5eca2de
Kadiga6 <9510@holbertonstudents.com>
2025-06-18 23:30:43 +0200

    update

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
-from .database.db import init_db
+from .database.db import db
 from .routes import user_routes, ai_routes
 
 def create_app():
     app = Flask(__name__)
 
-    app.config.from_object('config.py')
+    app.config.from_object('config')
     
-    init_db(app)
+    db.init_db(app)
 
     app.register_blueprint(user_routes.bp)
     app.register_blueprint(ai_routes.bp)
